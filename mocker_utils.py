1e938eba58cb4ceeeae8caf2eaec6b9358e867a9
code007\Franciz <912953887@qq.com>
2023-01-10 16:00:43 +0800

    增加测试用例

--- mocker_utils.py
+++ mocker_utils.py
@@ -309,15 +309,3 @@ class MockerExpressionException(Exception):
 
 
 mocker = Mocker()
-
-if __name__ == '__main__':
-    # for i in range(20):
-    #     print(i, mocker.basic.boolean(1, 5, False))
-    # print(mocker.mocker("@boolean(1, 5, False)"))
-    # print(mocker.mocker("@date('%Y-%m-%d %H:%M:%S', '+1d')"))
-    # print(mocker.mocker("@date('%Y-%m-%d %H:%M:%S', '+24h')"))
-    # print(mocker.date.date('%y-%m-%d', '-20d'))
-    # print(mocker.date.time('', '+2sec'))
-    # print(mocker.date.time('', '+4sec'))
-    print(mocker.Helper().pick('(1,2,3)'))
-    print(mocker.mocker("""@pick('("1",2,"3")')"""))
